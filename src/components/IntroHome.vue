8754326d6b6451f70173903e979a0ba8b3d75edd
Aamir Farooq <aamir@farooq.xyz>
2020-01-01 23:43:42 +0500

    Home: Add iamge, more structure layout

--- src/components/IntroHome.vue
+++ src/components/IntroHome.vue
@@ -1,54 +1,77 @@
 <template>
 	<div>
-		<section class="hero is-info is-large">
+		<section class="hero purple-gradient is-large">
 			<div class="hero-head">
-				<nav
-					class="navbar"
-					role="navigation"
-					aria-label="main navigation"
-				>
-					<div class="navbar-brand">
-						<a class="navbar-item" href="https://bulma.io">
-							Talkboard
-						</a>
-
-						<a
-							role="button"
-							class="navbar-burger burger"
-							aria-label="menu"
-							aria-expanded="false"
-							data-target="navbarBasicExample"
-						>
-							<span aria-hidden="true"></span>
-							<span aria-hidden="true"></span>
-							<span aria-hidden="true"></span>
-						</a>
-					</div>
-
-					<div id="navbarBasicExample" class="navbar-menu">
-						<div class="navbar-start">
-							<a class="navbar-item">
-								Home
-							</a>
-
-							<a class="navbar-item">
-								Documentation
-							</a>
-						</div>
-					</div>
-				</nav>
+				<Navbar></Navbar>
 			</div>
 			<div class="hero-body">
-				<div class="container"></div>
+				<div class="container">
+					<div class="flex-container">
+						<div class="img-container">
+							<img
+								src="https://is3-ssl.mzstatic.com/image/thumb/Music113/v4/ef/41/ea/ef41eadf-0200-6bcd-999b-7c8ac78d298c/source/600x600bb.jpg"
+								width="300"
+								height="300"
+							/>
+						</div>
+						<div class="text-container">
+							<p class="title">Hey</p>
+							<p class="subtitle">Yo</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
 	</div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
 export default {
-	name: "IntroHome"
+	name: "IntroHome",
+	components: {
+		Navbar
+	}
 };
 </script>
 
-<style></style>
+<style>
+.purple-gradient {
+	background: rgb(136, 46, 191);
+	background: linear-gradient(
+		180deg,
+		rgba(136, 46, 191, 1) 0%,
+		rgba(0, 0, 0, 1) 170%
+	);
+	/* box-shadow: inset 0em 0em 5em 0em rgb(49, 49, 49); */
+}
+.flex-container {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: row;
+	width: 100%;
+}
+.hero-body {
+	padding-top: 100px !important;
+	padding-bottom: 100px !important;
+}
+
+.img-container {
+	flex-grow: 1;
+	display: flex;
+	justify-content: center;
+}
+
+.img-container img {
+	border-radius: 20px;
+	box-shadow: 1em 1em 3em #000;
+}
+
+.text-container {
+	flex-grow: 1.5;
+}
+.text-container p {
+	color: #fff;
+}
+</style>
