4c703a6fe96d52bb463bc1d65662c2116ccc533e
Aamir Farooq <aamir@farooq.xyz>
2020-01-02 13:48:48 +0500

    Tweak colors, add where to listen badges

---
+++ src/components/WhereToListen.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="hero where-to-listen">
+        <div class="hero-body reset">
+            <div class="flex-container">
+                <p class="listen-on">Listen on:</p>
+                <img class="badge" src="@/assets/spotify-badge.png" />
+                <img class="badge" src="@/assets/apple-badge.png" />
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "WhereToListen"
+};
+</script>
+
+<style>
+.where-to-listen {
+    background-color: #111111;
+}
+
+.reset {
+    padding-top: 30px !important;
+    padding-bottom: 30px !important;
+}
+
+.badge {
+    height: 70px;
+    margin: 10px;
+}
+
+.listen-on {
+    color: #fff;
+    font-size: 2.5em;
+    margin-right: 30px;
+}
+</style>
