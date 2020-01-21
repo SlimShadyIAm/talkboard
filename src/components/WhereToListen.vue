917c49068bce0f6882e2f90e30fd68bc6d5dfcbc
Aamir Farooq <aamir@farooq.xyz>
2020-01-21 13:43:41 +0100

    Some color tweaks

--- src/components/WhereToListen.vue
+++ src/components/WhereToListen.vue
@@ -1,41 +1,36 @@
 <template>
-	<section class="hero where-to-listen">
-		<div class="hero-body reset">
-			<div class="columns">
-				<div class="column is-3">
-					<!-- <p class="listen-on">Listen on:</!-->
-
-					<div class="badges">
-						<a
-							href="https://open.spotify.com/show/1sCjmMpUDqnJNU54KwRf2Z"
-						>
-							<img
-								class="badge"
-								src="@/assets/spotify-badge.png"
-							/> </a
-						><br />
-						<a
-							href="https://podcasts.apple.com/gb/podcast/talkboard/id1469760640"
-							><img class="badge" src="@/assets/apple-badge.png"
-						/></a>
-						<div class="badge rss">
-							<a class="button copy-text">Copy RSS Link</a>
-							<a
-								class="button is-info copy-button"
-								v-clipboard:copy="message"
-							>
-								Copy
-							</a>
-						</div>
-					</div>
-				</div>
-				<div class="column">
-					<p class="title has-text-white">Latest episodes:</p>
-					<Episodes></Episodes>
-				</div>
-			</div>
-		</div>
-	</section>
+    <section class="hero where-to-listen">
+        <div class="hero-body reset">
+            <div class="container">
+                <div class="columns">
+                    <div class="column is-3">
+                        <!-- <p class="listen-on">Listen on:</!-->
+                        <p class="title has-text-white has-text-centered">Where to listen</p>
+                        <div class="badges">
+                            <a href="https://open.spotify.com/show/1sCjmMpUDqnJNU54KwRf2Z">
+                                <img class="badge" src="@/assets/spotify-badge.png" />
+                            </a>
+                            <br />
+                            <a href="https://podcasts.apple.com/gb/podcast/talkboard/id1469760640">
+                                <img class="badge" src="@/assets/apple-badge.png" />
+                            </a>
+                            <div class="badge rss">
+                                <a class="button copy-text">Copy RSS Link</a>
+                                <a
+                                    class="button is-info copy-button"
+                                    v-clipboard:copy="message"
+                                >Copy</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <p class="title has-text-white">Latest episodes</p>
+                        <Episodes></Episodes>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -46,62 +41,62 @@ VueClipboard.config.autoSetContainer = true; // add this line
 Vue.use(VueClipboard);
 
 export default {
-	name: "WhereToListen",
-	components: {
-		Episodes
-	},
-	data() {
-		return {
-			message: "https://rss.whooshkaa.com/rss/podcast/id/7841"
-		};
-	}
+    name: "WhereToListen",
+    components: {
+        Episodes
+    },
+    data() {
+        return {
+            message: "https://rss.whooshkaa.com/rss/podcast/id/7841"
+        };
+    }
 };
 </script>
 
 <style>
 .where-to-listen {
-	background-color: #111111;
+    background-color: #272829;
 }
 
 .reset {
-	padding-top: 30px !important;
-	padding-bottom: 50px !important;
+    padding-top: 30px !important;
+    padding-bottom: 50px !important;
 }
 
 .badges {
-	display: flex;
-	height: 100%;
-	justify-content: center;
-	flex-direction: column;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    height: 80%;
 }
 
 .badge {
-	width: 225px;
-	margin: 10px;
-	display: block;
-	margin: 0 auto;
+    width: 225px;
+    margin: 10px;
+    display: block;
+    margin: 0 auto;
 }
 
 .rss {
-	margin-top: 30px;
-	display: inline-block;
+    margin-top: 30px;
+    display: inline-block;
 }
 
 .copy-text {
-	border-top-right-radius: 0px;
-	border-bottom-right-radius: 0px;
-	width: 65%;
+    border-top-right-radius: 0px;
+    border-bottom-right-radius: 0px;
+    width: 65%;
 }
 
 .copy-button {
-	border-top-left-radius: 0px;
-	border-bottom-left-radius: 0px;
-	width: 35%;
+    border-top-left-radius: 0px;
+    border-bottom-left-radius: 0px;
+    width: 35%;
 }
 
 .listen-on {
-	color: #fff;
-	font-size: 2.5em;
-	margin-right: 30px;
+    color: #fff;
+    font-size: 2.5em;
+    margin-right: 30px;
 }
 </style>
