4eb46175e26d4ad236e5dfdaffdd98f2eb35d4cc
Aamir Farooq <aamir@farooq.xyz>
2020-01-21 14:28:41 +0100

    Add padding for text

--- src/components/HomeDetails.vue
+++ src/components/HomeDetails.vue
@@ -1,25 +1,26 @@
 <template>
-	<div class="container start">
-		<div class="columns">
-			<div class="column">
-				<p class="title is-spaced">About</p>
-				<Profile :src="aaron" name="23 Aaron"></Profile>
-				<p class="subtitle is-5">
-					A show hosted by 23 Aaron & sometimes a co-host!
-				</p>
-				<p class="subtitle is-5">
-					From week to week, there will be special guests, interesting
-					topics, and even community questions. If you'd like to take
-					part in the podcast, contact @TalkBoardLive on Twitter.
-				</p>
-			</div>
-			<br />
-			<div class="column">
-				<p class="title">Previous Guests</p>
-				<PreviousGuests></PreviousGuests>
-			</div>
-		</div>
-	</div>
+    <div class="container start">
+        <div class="columns">
+            <div class="column">
+                <p class="title is-spaced">About</p>
+                <Profile :src="aaron" name="23 Aaron"></Profile>
+                <p
+                    class="subtitle is-5"
+                    style="padding: 0 20px;"
+                >A show hosted by 23 Aaron & sometimes a co-host!</p>
+                <p class="subtitle is-5" style="padding: 0 20px;">
+                    From week to week, there will be special guests, interesting
+                    topics, and even community questions. If you'd like to take
+                    part in the podcast, contact @TalkBoardLive on Twitter.
+                </p>
+            </div>
+            <br />
+            <div class="column">
+                <p class="title">Previous Guests</p>
+                <PreviousGuests></PreviousGuests>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -27,30 +28,30 @@ import Profile from "@/components/Profile";
 import PreviousGuests from "@/components/PreviousGuests";
 import aaronImg from "@/assets/profiles/23 Aaron.png";
 export default {
-	components: {
-		Profile,
-		PreviousGuests
-	},
-	data() {
-		return {
-			aaron: aaronImg
-		};
-	}
+    components: {
+        Profile,
+        PreviousGuests
+    },
+    data() {
+        return {
+            aaron: aaronImg
+        };
+    }
 };
 </script>
 
 <style>
 .start {
-	margin-top: 30px;
-	margin-bottom: 30px;
+    margin-top: 30px;
+    margin-bottom: 30px;
 }
 
 .start .title {
-	text-align: center;
+    text-align: center;
 }
 
 .columns {
-	margin-left: 0rem;
-	margin-right: 0rem;
+    margin-left: 0rem;
+    margin-right: 0rem;
 }
 </style>
